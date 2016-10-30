11ed2cd70146f50d984a58669d6ff48450fd005a
Mario <mario@mariojia-MB01.local>
2016-10-30 23:28:45 +0800

    Initial commit

---
+++ ant2mvn/logger.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+    datefmt='%a, %d %b %Y %H:%M:%S',
+)
+
+def getLogger(name):
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    return log
